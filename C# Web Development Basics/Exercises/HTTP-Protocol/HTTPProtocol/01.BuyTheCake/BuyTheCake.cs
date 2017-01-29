60f256fd0f5c27a16fa4c0c8ae8f107cd7925bab
msmilkoff <msmilkoff@gmail.com>
2017-01-29 15:48:25 +0200

    Add stupid 'Cake' exercise with the ancient CGI

---
+++ C# Web Development Basics/Exercises/HTTP-Protocol/HTTPProtocol/01.BuyTheCake/BuyTheCake.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.BuyTheCake
+{
+    using System;
+    using System.IO;
+
+    public class BuyTheCake
+    {
+        public static void Main()
+        {
+            string htmlContent = File.ReadAllText("index.html");
+
+            Console.WriteLine("Content-type: text/html\n");
+            Console.WriteLine(htmlContent);
+        }
+    }
+}
---
+++ C# Web Development Basics/Exercises/HTTP-Protocol/HTTPProtocol/02.AddCake/AddCake.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.AddCake
+{
+    using System;
+    using System.IO;
+
+    public class AddCake
+    {
+        public static void Main()
+        {
+            var htmlContent = File.ReadAllText("add-cake.html");
+
+            Console.WriteLine("Content-type: text/html\n");
+            Console.WriteLine(htmlContent);
+
+            var cakeInfo = Console.ReadLine();
+            var keyValuePairs = cakeInfo.Split('&');
+            string cakeName = keyValuePairs[0].Split('=')[1];
+            string cakePrice = keyValuePairs[1].Split('=')[1];
+
+            File.AppendAllText("database.csv", $"{cakeName},{cakePrice}" + Environment.NewLine);
+        }
+    }
+}
