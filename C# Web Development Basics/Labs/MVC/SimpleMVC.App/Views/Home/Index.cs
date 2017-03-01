b5c822281c5ec73686dd48f34a09944604c4b3de
msmilkoff <msmilkoff@gmail.com>
2017-03-01 12:27:08 +0200

    Create simple "HEllo MVC" test

---
+++ C# Web Development Basics/Labs/MVC/SimpleMVC.App/Views/Home/Index.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleMVC.App.Views.Home
+{
+    using MVC.Interfaces;
+
+    public class Index : IRenderable
+    {
+        public string Render()
+        {
+            return "<h3>Hello MVC!</h3>";
+        }
+    }
+}
